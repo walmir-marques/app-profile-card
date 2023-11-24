6b5ce79f6b63df622b70bfc27b993f10fd81d8a9
Walmir Marques <walmir-mf@hotmail.com>
2023-11-24 01:53:41 -0300

    Meu primeiro projeto em react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <img className="avatar" src="./images/WalmirMarques.jpg" alt="Minha foto" />
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Walmir Marques Filho</h1>
+      <p>
+        Me chamo Walmir Marques Filho, sou formado em Meu nome é Walmir Marques
+        Filho, sou formado em ciência da computação, trabalhei na área de
+        suporte de TI por cerca de 5 anos e após 2 anos morando na Irlanda,
+        retornei ao Brasil e iniciei meus estudos na área de Front - End para
+        mudança carreira.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="HTML" emoji="📕" color="green" />
+      <Skill skill="CSS" emoji="📗" color="blue" />
+      <Skill skill="Javascrip" emoji="📘" color="red" />
+      <Skill skill="React" emoji="📚" color="green" />
+      <Skill skill="GitHub" emoji="📙" color="blue" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
